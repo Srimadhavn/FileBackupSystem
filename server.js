23e36f0f0b0c94ac4a0f83d4fb8e67a201efd445
srimadhavn <srimadhavan@aol.com>
2025-01-12 01:33:55 +0530

    update : deboucing

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const fs = require('fs')
+const path = require('path')
 
-fs.watch("source.txt",(eventType,filename)=>{
+function debounce(func, timeout = 300){
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+  }
+
+fs.watch("./files/source.txt",debounce((eventType,filename)=>{
     if(filename){
-        console.log('The file ${filename} has changed, event type : ${eventType}')
+        console.log(`The file ${filename} has changed, event type : ${eventType}`)
         backupFile(filename);
     }
-})
+    else console.log(`The file ${filename} doesn't exist`)
+},100));
 
 function backupFile(filename){
-    fs.copyFile("source.txt","backup.txt",(err) => {
+    const sourceFile = path.resolve('./files',filename)
+    const backupFile = path.resolve(`./files/backup`)
+
+    fs.copyFile(sourceFile,backupFile,(err) => {
         if(err) console.error(err);
         else console.log('File backed up successfully')
     })
